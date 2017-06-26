afb9dfecaac94b5f85196e1ad31069239054e9a8
Ally McGilloway <alastair.mcgilloway@codeclan.com>
2017-06-26 13:35:38 +0100

    Adds Wresting starter code

---
+++ app/src/main/java/db/SqlRunner.java
@@ -0,0 +1,82 @@
+package db;
+
+/**
+ * Created by user on 26/06/2017.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SqlRunner {
+
+    private static Connection c = null;
+    private static Statement stmt = null;
+
+    private static void connect() {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:./app/src/main/java/db/ifpwafcad.db");
+            c.setAutoCommit(false);
+            stmt = c.createStatement();
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        System.out.println("Opened database successfully");
+        System.out.println();
+
+    }
+
+    public static void closeConnection(){
+        try{
+            stmt.close();
+            c.close();
+        } catch (Exception e){
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static int executeUpdate(String sql) {
+        int id = 0;
+        try {
+            connect();
+            stmt.executeUpdate(sql);
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                id = (generatedKeys.getInt(1));
+            }
+
+            c.commit();
+
+
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Records created successfully");
+        System.out.println();
+        return id;
+    }
+
+    public static ResultSet executeQuery(String sql) {
+        ResultSet rs = null;
+        try {
+            connect();
+            rs = stmt.executeQuery(sql);
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+
+        System.out.println("Operation done successfully");
+        System.out.println();
+        return rs;
+    }
+
+}
---
+++ app/src/main/java/db/ifpwafcad.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS Counselor;
+DROP TABLE IF EXISTS Subject;
+
+
+CREATE TABLE wrestlers (
+       ID INTEGER PRIMARY KEY AUTOINCREMENT,
+       first_name VARCHAR (50),
+       nick_name VARCHAR (50),
+       last_name VARCHAR (50),
+       telephone VARCHAR (25),
+       email VARCHAR (50),
+       member_since DATE DEFAULT '0000-00-00'
+
+);
+
+
+CREATE TABLE subjects (
+	ID INTEGER PRIMARY KEY AUTOINCREMENT,
+	name VARCHAR (50),
+	description TEXT,
+	counselor_id INTEGER references Counselor(id)
+);
+
+
+INSERT INTO Counselor (first_name, nick_name, last_name, telephone, email, member_since)
+       VALUES  	('Jake', 'The Snake', 'Roberts', '412 565-5656', 'snake@ifpwafcad.com', '2003-09-10'),
+               	('Andre', 'The Giant', '', '606 443-4567', 'bobo@ifpwafcad.com', '2001-01-12'),
+               	('The', '', 'Undertaker', '555 555-4432', 'undertaker@ifpwafcad.com', '2005-03-08'),
+               	('Randy', 'Macho Man', 'Savage', '555 317-4444', 'machoman@hotmail.com', '2000-11-10'),
+               	('Steve', 'Stone Cold', 'Austen','334 612-5678', 'stonecold@ifpwafcad.com', '1996-01-01'),
+               	('Bret', 'The Hitman', 'Hart', '412 565-5656', 'hitman@ifpwafcad.com', date('now')),
+               	('Cactus', '', 'Jack', '553 499-8162', 'cactus_jack@ifpwafcad.com', '1999-12-03'),
+               	('Dwayne', 'The Rock', 'Johnson', '617 889-5545', 'rock@ifpwafcad.com', '1998-05-07'),
+               	('Brutus', '"The Barber"', 'BeefCake', '777 513-3333', 'brutus@ifpwafcad.com', '2002-07-09');
+
+
+INSERT INTO Subject (name, description, counselor_id)
+	VALUES  ('Financial Consultancy', 'Investment advice and financial planning guidance, helping you to maximize your net worth through proper asset allocation. This includes the stocks, bonds, mutual funds, insurance products, and gambling strategies proven to work.', '9'),
+		('Existential Psychotherapy', 'Often wonder what the purpose of life is? After learning the basics of Existential Psychotherapy, you''ll understand why you''re happy when you''re feeling happy, and why you''re not feeling happy when you''re not happy, allowing you to transcend to a state of pure bliss.', '7'),
+	        ('Temper Management', 'Are your angry outbursts affecting your relationships with loved-ones? Do tantrums at work hinder your ability to perform? Temper management helps you to channel your anger into positive, life-changing productivity.', '4'),
+		('Past-Life Regression', 'Past-Life Regression is a journey of the soul, backward and forward through time, like a yo-yo.', '2'),
+	       	('Marriage Guidance', 'Even if you share a solid, caring and mutually beneficial relationship with your spouse, you may both still need urgent counseling. There''s only one way to find out. Contact us now!', '1'),
+	       	('Crisis Management', 'Whether you''re a fireman, executive CEO, or housewife, applying crisis management techniques at the right moment can be life-saving for you as well as all those around you.', '3'),
+	       	('Dream Analysis', 'Dream Analysis will allow you to delve into the depths of your subconcious. Your counselor will put you through a rigorous, disciplined training program, allowing you to remain in a waking state while dreaming. By the end, you''ll be able to analyse your dreams while you are having them!', '8'),
+	       	('Hypnosis', 'Contrary to popular belief, hypnosis can be a powerful and effective form of counseling.', '6'),
+		('Reiki', 'Need a massage but are afraid to let a stranger touch your body? Reiki could be the perfect solution for you.', '5');
